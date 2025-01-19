b100470c24d3a7bb1935b570b287e812daa5a65f
nithya4648 <nithya4648@gmail.com>
2025-01-19 13:24:42 +0530

    changes in home page

--- home.js
+++ home.js
@@ -1,45 +1,60 @@
-function openURL() {
-  var url = "Resume.pdf";
+//preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
 
-  window.open(url, "_blank");
-}
-let mybutton = document.getElementById("backtotopbutton");
+//mobile nav
+        var theToggle = document.getElementById('toggle');
+        var menu = document.getElementById('menu');
+        var lastScrollTop = 0;
+        // Toggle button and menu display
+        theToggle.onclick = function () {
+            toggleClass(this, 'on');
+            toggleClass(menu, 'show');
+        };
+        function hasClass(elem, className) {
+            return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+        }
+        function addClass(elem, className) {
+            if (!hasClass(elem, className)) {
+                elem.className += ' ' + className;
+            }
+        }
+        function removeClass(elem, className) {
+            var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+            if (hasClass(elem, className)) {
+                while (newClass.indexOf(' ' + className + ' ') >= 0) {
+                    newClass = newClass.replace(' ' + className + ' ', ' ');
+                }
+                elem.className = newClass.replace(/^\s+|\s+$/g, '');
+            }
+        }
+        function toggleClass(elem, className) {
+            var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
+            if (hasClass(elem, className)) {
+                while (newClass.indexOf(" " + className + " ") >= 0) {
+                    newClass = newClass.replace(" " + className + " ", " ");
+                }
+                elem.className = newClass.replace(/^\s+|\s+$/g, '');
+            } else {
+                elem.className += ' ' + className;
+            }
+        }
+        // Hide menu on scroll down, show on scroll up
+        window.addEventListener("scroll", function () {
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            if (scrollTop > lastScrollTop) {
+                // Scrolling down
+                menu.classList.remove('show');
+                theToggle.classList.remove('on');
+            }
+            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For mobile or negative scrolling
+        });
 
-// Monitor scroll events
-window.onscroll = function () {
-    scrollFunction();
-};
 
-function scrollFunction() {
-    // Show the button only when scrolled past 400px
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
 
-// Scroll to top when the button is clicked
+// back to top button
 function scrolltoTopfunction() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
-
-
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent the form from refreshing the page
-
-  // Get form data
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-
-  // Simple validation
-  if (name && email && message) {
-      // Show confirmation message
-      document.getElementById('confirmationMessage').style.display = 'block';
-
-      // Clear the form (optional)
-      document.getElementById('contactForm').reset();
-  }
-});
-
